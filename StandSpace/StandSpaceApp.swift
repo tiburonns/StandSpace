f74a90e6d805a747b1041a6918fbbab323e6369f
tiburonns <5pjvjmckn7@privaterelay.appleid.com>
2026-09-19 11:13:32 -0600

    feat: apply persisted StandSpace language to SwiftUI locale

--- StandSpace/StandSpaceApp.swift
+++ StandSpace/StandSpaceApp.swift
@@ -3,11 +3,18 @@ import SwiftUI
 @main
 struct StandSpaceApp: App {
     @StateObject private var store = DashboardStore()
+    @AppStorage("standspace.app.language")
+    private var languageRawValue = StandSpaceAppLanguage.system.rawValue
+
+    private var language: StandSpaceAppLanguage {
+        StandSpaceAppLanguage(rawValue: languageRawValue) ?? .system
+    }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(store)
+                .environment(\.locale, language.locale)
                 .preferredColorScheme(.dark)
         }
     }
